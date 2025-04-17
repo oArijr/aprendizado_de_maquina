e6d555b7fc818dbf35221968da74c49712d3a347
Luigi G. Marchetti <luigigarciamarchetti@outlook.com>
2025-04-16 21:06:42 -0300

    Polinomial regression finished

--- Análise de Correlação e Regressão Linear/regressao_polinomial/demo_regressaop.py
+++ Análise de Correlação e Regressão Linear/regressao_polinomial/demo_regressaop.py
@@ -27,20 +27,20 @@ ax.set_title("Normal")
 ax.scatter(x, y)
 
 #C)
-list_n1 = regressao_polinomial(x, y, 1)
-ax.plot(x , list_n1, c='r')
+list_n1_train = regressao_polinomial(x, y, 1)
+ax.plot(x, list_n1_train, c='r')
 
 #D)
-list_n2 = regressao_polinomial(x, y, 2)
-ax.plot(x , list_n2, c='g')
+list_n2_train = regressao_polinomial(x, y, 2)
+ax.plot(x, list_n2_train, c='g')
 
 #E)
-list_n3 = regressao_polinomial(x, y, 3)
-ax.plot(x , list_n3, c='black')
+list_n3_train = regressao_polinomial(x, y, 3)
+ax.plot(x, list_n3_train, c='black')
 
 #F)
-list_n8 = regressao_polinomial(x, y, 8)
-ax.plot(x, list_n8, c='y')
+list_n8_train = regressao_polinomial(x, y, 8)
+ax.plot(x, list_n8_train, c='y')
 
 plt.show()
 
@@ -57,20 +57,19 @@ def erro_quadratico_medio(y_linear, y):
 
     y_tam = len(y_linear)
 
-    res = soma_residuos / y_tam
-    return res
+    return soma_residuos / y_tam
 
 print("\nEQM 1")
-print(erro_quadratico_medio(list_n1, y))
+print(round(erro_quadratico_medio(list_n1_train, y), 5))
 
 print("EQM 2")
-print(erro_quadratico_medio(list_n2, y))
+print(round(erro_quadratico_medio(list_n2_train, y), 5))
 
 print("EQM 3")
-print(erro_quadratico_medio(list_n3, y))
+print(round(erro_quadratico_medio(list_n3_train, y), 5))
 
 print("EQM 8")
-print(erro_quadratico_medio(list_n8, y))
+print(round(erro_quadratico_medio(list_n8_train, y), 5))
 print()
 
 #H)
@@ -90,66 +89,61 @@ ax = fig2.add_subplot()
 ax.set_title("\nTreino")
 ax.scatter(x_train, y_train)
 
-list_n1 = regressao_polinomial(x_train, y_train, 1)
-ax.plot(x_train , list_n1, c='r')
+list_n1_train = regressao_polinomial(x_train, y_train, 1)
+ax.plot(x_train, list_n1_train, c='r')
 
-list_n2 = regressao_polinomial(x_train, y_train, 2)
-ax.plot(x_train , list_n2, c='g')
+list_n2_train = regressao_polinomial(x_train, y_train, 2)
+ax.plot(x_train, list_n2_train, c='g')
 
-list_n3 = regressao_polinomial(x_train, y_train, 3)
-ax.plot(x_train , list_n3, c='black')
+list_n3_train = regressao_polinomial(x_train, y_train, 3)
+ax.plot(x_train, list_n3_train, c='black')
 
-list_n8 = regressao_polinomial(x_train, y_train, 8)
-ax.plot(x_train, list_n8, c='y')
+list_n8_train = regressao_polinomial(x_train, y_train, 8)
+ax.plot(x_train, list_n8_train, c='y')
 
 plt.show()
 
 #J)
-# Obter os coeficientes dos modelos treinados
-coef_n1_test = regressao_polinomial(x_test, y_test, 1)
-ax.plot(x_train , list_n1, c='r')
-
-coef_n2_test = regressao_polinomial(x_test, y_test, 2)
-ax.plot(x_train , list_n2, c='g')
-
-coef_n3_test = regressao_polinomial(x_test, y_test, 3)
-ax.plot(x_train , list_n3, c='black')
-
-coef_n8_test = regressao_polinomial(x_test, y_test, 8)
-ax.plot(x_train, list_n8, c='y')
-
 # Calculando EQM para dados de teste
 print("\nEQM nos dados de TESTE:")
-print("EQM 1:", erro_quadratico_medio(coef_n1_test, y_test))
-print("EQM 2:", erro_quadratico_medio(coef_n2_test, y_test))
-print("EQM 3:", erro_quadratico_medio(coef_n3_test, y_test))
-print("EQM 8:", erro_quadratico_medio(coef_n8_test, y_test))
+print("EQM 1:", round(erro_quadratico_medio(list_n1_train, y_test), 5))
+print("EQM 2:", round(erro_quadratico_medio(list_n2_train, y_test), 5))
+print("EQM 3:", round(erro_quadratico_medio(list_n3_train, y_test), 5))
+print("EQM 8:", round(erro_quadratico_medio(list_n8_train, y_test), 5))
 
 #K)
 # Calculando R2 para os dados de treino
-r2_train_n1 = r2_score(y_train, list_n1)
-r2_train_n2 = r2_score(y_train, list_n2)
-r2_train_n3 = r2_score(y_train, list_n3)
-r2_train_n8 = r2_score(y_train, list_n8)
+r2_train_n1 = r2_score(y_train, list_n1_train)
+r2_train_n2 = r2_score(y_train, list_n2_train)
+r2_train_n3 = r2_score(y_train, list_n3_train)
+r2_train_n8 = r2_score(y_train, list_n8_train)
 
-print("\nR2 nos dados de TREINO:")
-print("R2 N=1:", r2_train_n1)
-print("R2 N=2:", r2_train_n2)
-print("R2 N=3:", r2_train_n3)
-print("R2 N=8:", r2_train_n8)
+print("\nR2 nos dados de treino:")
+print("R2 N=1:", round(r2_train_n1, 5))
+print("R2 N=2:", round(r2_train_n2, 5))
+print("R2 N=3:", round(r2_train_n3, 5))
+print("R2 N=8:", round(r2_train_n8, 5))
+
 
 # Calculando R2 para os dados de teste
-r2_test_n1 = r2_score(y_test, coef_n1_test)
-r2_test_n2 = r2_score(y_test, coef_n2_test)
-r2_test_n3 = r2_score(y_test, coef_n3_test)
-r2_test_n8 = r2_score(y_test, coef_n8_test)
+list_n1_test = regressao_polinomial(x_test, y_test, 1)
+list_n2_test = regressao_polinomial(x_test, y_test, 2)
+list_n3_test = regressao_polinomial(x_test, y_test, 3)
+list_n8_test = regressao_polinomial(x_test, y_test, 8)
 
-print("\nR2 nos dados de TESTE:")
-print("R2 N=1:", r2_test_n1)
-print("R2 N=2:", r2_test_n2)
-print("R2 N=3:", r2_test_n3)
-print("R2 N=8:", r2_test_n8)
+r2_test_n1 = r2_score(y_test, list_n1_test)
+r2_test_n2 = r2_score(y_test, list_n2_test)
+r2_test_n3 = r2_score(y_test, list_n3_test)
+r2_test_n8 = r2_score(y_test, list_n8_test)
+
+print("\nR2 nos dados de teste:")
+print("R2 N=1:", round(r2_test_n1, 5))
+print("R2 N=2:", round(r2_test_n2, 5))
+print("R2 N=3:", round(r2_test_n3, 5))
+print("R2 N=8:", round(r2_test_n8, 5))
 
 
-# O modelo N8 em geral. Em alguns cenários de testes, devido principalmente a serem poucos casos, isso pode variar,
-# com o N3 por exemplo desempenhando melhor
+#I)
+# O modelo N8 aparenta ser o melhor porém acreditamos que ele se encaixa no overfitting.
+# Em alguns cenários de testes, o N8 acaba tendo performando muito mal, mostrando sua vulnerabilidade.
+# Já o N3 por ser mais equilibrado acaba performando melhor com os dados de teste em média.
