17ce7733c2f5f1645385aa7009acdadb8e0a9ea1
Luigi G. Marchetti <luigigarciamarchetti@outlook.com>
2025-04-20 17:10:25 -0300

    Knn continuation

--- K-Nearest Neighbour/knn/Utils.py
+++ K-Nearest Neighbour/knn/Utils.py
@@ -1,6 +1,27 @@
 import math
 import numpy as np
-def distancia_euclidiana(x1, x2, y1, y2):
-    return math.sqrt(sum(np.pow(x2 - x1, 2) , np.pow(y2 - y1, 2)))
+from scipy import stats
 
-def classificador():
+
+def dist(vetor_p, vetor_q):
+    total = 0
+    for p, q in zip(vetor_p, vetor_q):
+        total += np.pow(p - q, 2)
+    return math.sqrt(total)
+
+
+def meuKnn(dadosTrain, rotuloTrain, dadosTeste, k):
+    for i in dadosTeste:
+        distancias = []
+        for j in dadosTrain:
+            # % Calcule a distância entre o exemplo de teste e os dados de treinamento
+            distancias.append(dist(dadosTrain[j], dadosTeste[i]))
+
+        distOrdenada = sorted(distancias)
+
+        mais_proximos = distOrdenada[:k]
+
+        moda = stats.mode(mais_proximos)
+        res = moda.mode[0]
+        print(f"Moda teste {i}: ", res)
+        print(f"Rótulo teste {i}: ", rotuloTrain[i])
--- K-Nearest Neighbour/knn/demoD1.py
+++ K-Nearest Neighbour/knn/demoD1.py
@@ -1,5 +1,5 @@
-
-
+import scipy.io as scipy
+import Utils.meuKnn as meuKnn
 
 
 
@@ -7,3 +7,13 @@
 mat = scipy.loadmat('grupoDados1.mat')
 
 grupoTest = mat['grupoTest']
+grupoTrain = mat['grupoTrain']
+testRots = mat['testRots']
+trainRots = mat['trainRots']
+print(grupoTest)
+print(grupoTrain)
+print(testRots)
+print(trainRots)
+
+
+meuKnn(grupoTrain, testRots, grupoTest, 3)
